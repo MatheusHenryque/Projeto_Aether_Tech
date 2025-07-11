24a1ee7a7cdd85be179a0b1c5cb0762ebb5764e0
MatheusHenryque <mth123922@gmail.com>
2025-07-11 14:32:31 -0300

    Atualizações para Render

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/api/ping")
+def ping():
+    return jsonify({"status": "ok"})
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))

1b445a09e937fa60c29cd77e44f69a93ea4ee8c2
MatheusHenryque <mth123922@gmail.com>
2025-07-11 17:44:50 -0300

    Correção do header mobile

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, jsonify, request
-from flask_cors import CORS
+# from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 @app.route("/")
 def index():
